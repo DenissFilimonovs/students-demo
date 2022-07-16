fa6c1073efe2ddb83bb1c4bc703b0dc3bbe3f2f8
Deniss Filimonovs <deniss20025@gmail.com>
2022-07-16 14:17:31 +0300

    init commit

--- src/index.ts
+++ src/index.ts
@@ -69,6 +69,7 @@ app.delete('/videos/:id',(req:Request,res:Response) => {
     for(let i=0; i<videos.length;i++) {
         if(videos[i].id === Number(req.params.id)) {
             videos.splice(i,1)
+            res.send(204)
             return
         }
     }
